67dadd2f09aad6fee932d907504c03ffc4587bf0
BryanD <vercelli.bryan92@gmail.com>
2022-02-10 11:39:28 +0700

    week 1 test

---
+++ app/src/main/java/com/ubaya/native160419039_160419043_160419073/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.native160419039_160419043_160419073
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/ubaya/native160419039_160419043_160419073/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.ubaya.native160419039_160419043_160419073
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
